9019accca3bb2390d5bd3b7a0b36c0d9bb141664
Rommel Rico <rommeltj@gmail.com>
2016-02-19 14:14:47 -0800

    Fixed issue with black bars on top and bottom
    The Parse Starter project was migrated from earlier version of Xcode.
    It removes the Launch storyboard/xib. I readied one and added it to the
    App Target properties.

--- ParseStarterProject/AppDelegate.swift
+++ ParseStarterProject/AppDelegate.swift
@@ -33,8 +33,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         PFACL.setDefaultACL(defaultACL, withAccessForCurrentUser: true)
 
         if application.applicationState != UIApplicationState.Background {
-            // Track an app open here if we launch with a push, unless
-            // "content_available" was used to trigger a background push (introduced in iOS 7).
+            // Track an app open here if we launch with a push, unless "content_available" was used to trigger a background push (introduced in iOS 7).
             // In that case, we skip tracking here to avoid double counting the app-open.
             let preBackgroundPush = !application.respondsToSelector("backgroundRefreshStatus")
             let oldPushHandlerOnly = !self.respondsToSelector("application:didReceiveRemoteNotification:fetchCompletionHandler:")
