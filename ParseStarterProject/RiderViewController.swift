c3cc6e404dadcbb1455c282c94c1a110631f7624
Rommel Rico <rommeltj@gmail.com>
2016-02-19 14:03:22 -0800

    Calling / Canceling Uber functionality
    Created Object in Parse called “RiderRequest” with username and
    location. A property in RiderViewController called riderRequestActive
    monitors if they are calling an uber or canceling an Uber.

--- ParseStarterProject/RiderViewController.swift
+++ ParseStarterProject/RiderViewController.swift
@@ -15,6 +15,10 @@ class RiderViewController: UIViewController, CLLocationManagerDelegate, MKMapVie
     //Properties
     @IBOutlet weak var map: MKMapView!
     var locationManager: CLLocationManager!
+    var latitude: CLLocationDegrees!
+    var longitude: CLLocationDegrees!
+    @IBOutlet weak var callAnUberButton: UIButton!
+    var riderRequestActive: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,20 +42,55 @@ class RiderViewController: UIViewController, CLLocationManagerDelegate, MKMapVie
     }
     
     @IBAction func callAnUber(sender: AnyObject) {
-        print("TODO: Call an Uber")
+        if riderRequestActive == false {
+            let riderRequest = PFObject(className:"RiderRequest")
+            riderRequest["username"] = PFUser.currentUser()?.username
+            riderRequest["location"] = PFGeoPoint(latitude: latitude, longitude: longitude)
+            riderRequest.saveInBackgroundWithBlock {
+                (success: Bool, error: NSError?) -> Void in
+                if (success) {
+                    // The object has been saved.
+                    self.callAnUberButton.setTitle("Cancel An Uber", forState: .Normal)
+                    self.riderRequestActive = true
+                } else {
+                    let alert = UIAlertController(title: "Could not call Uber", message: "Please try again later", preferredStyle: .Alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+                    self.presentViewController(alert, animated: true, completion: nil)
+                }
+            }
+        } else {
+            riderRequestActive = false
+            self.callAnUberButton.setTitle("Call An Uber", forState: .Normal)
+            let query = PFQuery(className:"RiderRequest")
+            query.whereKey("username", equalTo: (PFUser.currentUser()?.username)!)
+            query.findObjectsInBackgroundWithBlock({ (objects: [PFObject]?, error: NSError?) -> Void in
+                if error == nil {
+                    print("Successfully retrieved \(objects?.count) requests.")
+                    if let objects = objects {
+                        for object in objects {
+                            object.deleteInBackground()
+                        }
+                    }
+                } else {
+                    print("Error: \(error), \(error?.userInfo)")
+                }
+            })
+        }
     }
     
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         //Set the map region to current location
         let locValue: CLLocationCoordinate2D = (manager.location?.coordinate)!
-        let center = CLLocationCoordinate2D(latitude: locValue.latitude, longitude: locValue.longitude)
+        latitude = locValue.latitude
+        longitude = locValue.longitude
+        let center = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
         let span = MKCoordinateSpanMake(0.01, 0.01)
         let region = MKCoordinateRegion(center: center, span: span)
         self.map.setRegion(region, animated: true)
         
         //Add a pin to user location.
         self.map.removeAnnotations(self.map.annotations)
-        let pinLocation = CLLocationCoordinate2DMake(locValue.latitude, locValue.longitude)
+        let pinLocation = CLLocationCoordinate2DMake(latitude, longitude)
         let objectAnnotation = MKPointAnnotation()
         objectAnnotation.coordinate = pinLocation
         objectAnnotation.title = "My Location"
