06aa0976affefea6ff2a079f22ccd579b6584e4c
Rommel Rico <rommeltj@gmail.com>
2016-02-20 23:02:27 -0800

    Querying for closer requests for the driver
    Rider requests are queried by location.

--- ParseStarterProject/DriverTableViewController.swift
+++ ParseStarterProject/DriverTableViewController.swift
@@ -15,29 +15,21 @@ class DriverTableViewController: UITableViewController, CLLocationManagerDelegat
     //Properties
     var usernames = [String]()
     var locations = [CLLocationCoordinate2D]()
+    var locationManager: CLLocationManager!
+    var latitude: CLLocationDegrees!
+    var longitude: CLLocationDegrees!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let query = PFQuery(className:"RiderRequest")
-        query.findObjectsInBackgroundWithBlock({ (objects: [PFObject]?, error: NSError?) -> Void in
-            if error == nil {
-                //Successfully retrieved objects.
-                if let objects = objects {
-                    for object in objects {
-                        if let username = object["username"] as? String {
-                            self.usernames.append(username)
-                        }
-                        if let point = object["location"] as? PFGeoPoint {
-                            self.locations.append(CLLocationCoordinate2DMake(point.latitude, point.longitude))
-                        }
-                    }
-                    self.tableView.reloadData()
-                }
-            } else {
-                print("Error: \(error), \(error?.userInfo)")
-            }
-        })
+        //Location manager
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        
         
     }
 
@@ -61,43 +53,45 @@ class DriverTableViewController: UITableViewController, CLLocationManagerDelegat
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
         cell.textLabel?.text = usernames[indexPath.row]
+                                + " "
+                                + String(locations[indexPath.row].latitude)
+                                + ", "
+                                + String(locations[indexPath.row].longitude)
         return cell
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //Set the map region to current location
+        let locValue: CLLocationCoordinate2D = (manager.location?.coordinate)!
+        latitude = locValue.latitude
+        longitude = locValue.longitude
+        
+        //Query for Rider Requests and Update Table.
+        let query = PFQuery(className:"RiderRequest")
+        query.whereKey("location", nearGeoPoint: PFGeoPoint(latitude: latitude, longitude: longitude))
+        query.limit = 10
+        query.findObjectsInBackgroundWithBlock({ (objects: [PFObject]?, error: NSError?) -> Void in
+            if error == nil {
+                //Successfully retrieved objects.
+                if let objects = objects {
+                    self.usernames.removeAll()
+                    self.locations.removeAll()
+                    for object in objects {
+                        if let username = object["username"] as? String {
+                            self.usernames.append(username)
+                        }
+                        if let point = object["location"] as? PFGeoPoint {
+                            self.locations.append(CLLocationCoordinate2DMake(point.latitude, point.longitude))
+                        }
+                    }
+                    self.tableView.reloadData()
+                }
+            } else {
+                print("Error: \(error), \(error?.userInfo)")
+            }
+        })
+        
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     // MARK: - Navigation
 
